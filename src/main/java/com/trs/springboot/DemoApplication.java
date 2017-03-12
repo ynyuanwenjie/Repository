93f77453c54ca2b0ea65f294549bf74c6e53c28f
袁文界 <yuan.wenjie@trs.com.cn>
2017-03-12 18:11:40 +0800

    启动前初始化数据

--- src/main/java/com/trs/springboot/DemoApplication.java
+++ src/main/java/com/trs/springboot/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.trs.springboot;
 
+import com.trs.springboot.service.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by yuanwenjie on 2016/11/23.
@@ -9,6 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+    @Bean
+    CommandLineRunner runner(StudentRepository sr) {
+        return args -> {
+            sr.findByAddress("address26").stream().forEach(System.out::println);
+        };
+    };
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class,args);
     }
